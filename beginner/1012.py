519fab599fb655e6b487fb549bc289e6b3f9af43
Matheus Mota <smota.matheus@gmail.com>
2017-05-17 12:41:45 -0300

    Create 1012.py

---
+++ beginner/1012.py
@@ -0,0 +1,10 @@
+i = input().split(" ")
+a = float(i[0])
+b = float(i[1])
+c = float(i[2])
+
+print("TRIANGULO: %.3f" % ((a * c) / 2))
+print("CIRCULO: %.3f" % (3.14159 * (c ** 2)))
+print("TRAPEZIO: %.3f" % (((a + b) / 2) * c))
+print("QUADRADO: %.3f" % (b ** 2))
+print("RETANGULO: %.3f" % (a * b))
