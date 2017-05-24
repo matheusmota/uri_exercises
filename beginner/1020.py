e992ce9b4e5f4bfa62c48e53a35dfe15c8f38010
Matheus Mota <smota.matheus@gmail.com>
2017-05-24 16:55:55 -0300

    Update 1020.py

--- beginner/1020.py
+++ beginner/1020.py
@@ -1,1 +1,13 @@
+v = int(input())
 
+y = v // 365
+v = v - (y * 365)
+
+m = v // 30
+v = v - (m * 30)
+
+d = v
+
+print("{0} ano(s)" .format(y))
+print("{0} mes(es)".format(m))
+print("{0} dia(s)" .format(d))
