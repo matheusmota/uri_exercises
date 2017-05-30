d15cdc3477263b0fd6be47a6bdd541a20f0f254f
Matheus Mota <smota.matheus@gmail.com>
2017-05-30 17:21:56 -0300

    Update 1041.py

--- beginner/1041.py
+++ beginner/1041.py
@@ -2,9 +2,9 @@ x, y = [float(i) for i in input().split()]
 
 if (x == 0 and y == 0):
     print("Origem")
-elif (x == 0 and y != 0):
+elif (y == 0):
     print("Eixo X")
-elif (x != 0 and y == 0):
+elif (x == 0):
     print("Eixo Y")
 elif (x > 0 and y > 0):
     print("Q1")
